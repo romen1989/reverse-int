ae3afa9dcb198edaaaa47662e95ebfda7c5e4152
Yurkevich <roman.yurkevich@globant.com>
2021-08-23 16:53:57 +0300

    feat: added solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let result = 0;
+  n = Math.abs(n);
+  while(n > 0) {
+      result = result *10 + (n % 10);
+      n = Math.trunc(n/10);
+  }
+  return result;
 }
